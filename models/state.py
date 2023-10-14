14e20ae3f52d529219b47ac16cbc0e5c273b61bc
nada <nadaait171@gmail.com>
2023-10-14 06:38:00 -0700

    function details

--- models/state.py
+++ models/state.py
@@ -3,3 +3,10 @@
 from models.base_model import BaseModel
 
 class State(BaseModel):
+    """Represent a state
+
+    Attributes:
+        name (str) : the name of the state
+    """
+
+    name = ""
