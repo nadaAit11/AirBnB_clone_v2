cc7a46b86a55495c86931ada2d8b602b4f2babce
nada <nadaait171@gmail.com>
2023-10-14 06:27:24 -0700

    function details

--- models/place.py
+++ models/place.py
@@ -18,3 +18,15 @@ class Place(BaseModel):
         longitude (float) : the longitude of the place
         amenity_ids (list) : a list of amenity ids
     """
+
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
