a100c16b32627512eec55a517bd4cb58184d3df6
nada <nadaait171@gmail.com>
2023-10-14 06:33:20 -0700

    add description to the function

--- models/review.py
+++ models/review.py
@@ -3,4 +3,10 @@
 from models.base_model import BaseModel
 
 class Review(BaseModel):
+    """Represent a review
 
+    Attributes:
+        place_id (str) : the place id
+        user_id (str) : the user id
+        text (str) ! the text of the review
+    """
