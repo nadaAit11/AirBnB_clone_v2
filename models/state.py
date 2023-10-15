9abe2bc58f1e50896b32d115cb59c6fe43d3bcb3
NEAZYIT <touihnazih28@gmail.com>
2023-10-15 07:47:33 -0700

    fix bugs uniit tests

--- models/state.py
+++ models/state.py
@@ -5,10 +5,10 @@ from models.base_model import BaseModel
 
 class State(BaseModel):
     """
-    Represents a state.
+    Represents a state in the system.
 
     Attributes:
-        name (str): The name of the state.
+        name (str): The official name of the state.
     """
 
     name = ""
