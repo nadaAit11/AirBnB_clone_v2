44c38e9599c39db7e427763bf5adc83ac444aac3
NEAZYIT <touihnazih28@gmail.com>
2023-10-15 07:50:44 -0700

    fix bugs uniit tests

--- models/user.py
+++ models/user.py
@@ -10,8 +10,8 @@ class User(BaseModel):
     Attributes:
         email (str): The email of the user.
         password (str): The password of the user.
-        first_name (str): The first name of the user.
-        last_name (str): The last name of the user.
+        first_name (str): The user's first name.
+        last_name (str): The user's last name.
     """
 
     # Initialize empty strings for user information
