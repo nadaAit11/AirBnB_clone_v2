c8df76d1516388f8b0481eb93cdc5cdb7784889a
NEAZYIT <touihnazih28@gmail.com>
2023-11-25 19:58:51 +0100

    Implemented the Place class with attributes corresponding to columns in the 'places' table. Included city_id and user_id as foreign keys, specified various property attributes such as name, description, number of rooms and bathrooms, max_guest, price_by_night, latitude, and longitude. Default values and nullability settings were adjusted accordingly.

--- models/place.py
+++ models/place.py
@@ -1,33 +1,40 @@
 #!/usr/bin/python3
-""" Defines the Place class """
-from models.base_model import BaseModel
+"""
+Defines the Place class.
+"""
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
-class Place(BaseModel):
-    """Represent a place
+class Place(BaseModel, Base):
+    """
+    Represent a place.
 
     Attributes:
-        city_id (str): The city id.
-        user_id (str) : The user id.
-        name (str) : The name of the place.
-        description (str) : The description of the place.
-        number_rooms (int) : The number of rooms in the place.
-        number_bathrooms (int) : The number of bathrooms in the place.
-        max_guest (int) : The maximum number of guests of the place.
-        price_by_night (int) : The price by night of the place.
-        latitude (float) : The latitude of the place.
-        longitude (float) : The longitude of the place.
-        amenity_ids (list) : A list of Amenity ids.
+        __tablename__ (str): The name of the MySQL table to store Places.
+        city_id (Column): The city id of the Place.
+        user_id (Column): The user id of the Place.
+        name (Column): The name of the Place.
+        description (Column): The description of the Place.
+        number_rooms (Column): The number of rooms in the Place.
+        number_bathrooms (Column): The number of bathrooms in the Place.
+        max_guest (Column): The maximum number of guests of the Place.
+        price_by_night (Column): The price by night of the Place.
+        latitude (Column): The latitude of the Place.
+        longitude (Column): The longitude of the Place.
+        amenity_ids (list): A list of Amenity ids.
     """
+    __tablename__ = 'places'
 
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
