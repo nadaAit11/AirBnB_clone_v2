ff21ea975fd81691a0c5b07102ccf57f78d7253a
NEAZYIT <touihnazih28@gmail.com>
2023-11-25 20:04:42 +0100

    The code adds a relationship attribute to the User class named 'places', establishing a link with the Place class. When a User object is deleted, all related Place objects are automatically removed, and the reference from a Place object to its User is named 'user'.

--- models/user.py
+++ models/user.py
@@ -4,6 +4,8 @@ Defines the User class.
 """
 from sqlalchemy import Column, String
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from models.place import Place
 
 
 class User(BaseModel, Base):
@@ -16,6 +18,7 @@ class User(BaseModel, Base):
         password (Column): The password of the user.
         first_name (Column): The user's first name.
         last_name (Column): The user's last name.
+        places (relationship): The User-Place relationship.
     """
     # Represents the table name, users
     __tablename__ = 'users'
@@ -30,6 +33,9 @@ class User(BaseModel, Base):
 
     # Represents a column containing a string (128 characters) and can be null
     first_name = Column(String(128), nullable=True)
-    
+
     # Represents a column containing a string (128 characters) and can be null
     last_name = Column(String(128), nullable=True)
+
+    # Represents a relationship with the class Place
+    places = relationship("Place", backref="user", cascade="all, delete")
